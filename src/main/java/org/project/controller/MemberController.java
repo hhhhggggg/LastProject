080621b35d4a3edd015346a235a4d888c996e51d
young-joo-k <joo950330@gmail.com>
2023-09-04 20:07:05 +0900

    하하하

--- src/main/java/org/project/controller/MemberController.java
+++ src/main/java/org/project/controller/MemberController.java
@@ -78,7 +78,7 @@ public class MemberController {
 	    return "/join/index";
 	}
 
-	// 헤헤헤헿하핳후
+	// 헤헤헤헿하핳ㅎㅎㅎㅎㅎㅎ
 	@GetMapping("/index")
 	public void index(HttpSession session, Model model) {
 		String id = (String) session.getAttribute("id");
